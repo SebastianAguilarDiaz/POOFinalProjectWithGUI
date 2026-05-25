1c7aa81512bb5d8a17d38ac19150b0a8edfd2948
Sebatian Luis Enrique Aguilar Diaz <sebastian.luis.aguilar.diaz@gmail.com>
2026-05-25 11:09:15 -0600

    project config

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,4 +4,6 @@ module com.github.sym {
 
     opens com.github.sym to javafx.fxml;
     exports com.github.sym;
+    opens com.github.sym.UnoBasicGame to javafx.fxml;
+    exports com.github.sym.UnoBasicGame;
 }
